575c6e11cb03604f6cfa4e04b4063bec4314aedb
Diego Torres <torressdiiego@outlook.com>
2021-01-25 14:23:34 -0300

    Adaptação tela login e menu lateral

--- Sistema Administrativo/resources/views/layouts/menu-lateral.blade.php
+++ Sistema Administrativo/resources/views/layouts/menu-lateral.blade.php
@@ -8,11 +8,21 @@
     <div class="sidebar">
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
+            <!--<div class="image">
                 <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
+            </div>-->
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">{{\Illuminate\Support\Facades\Auth::user()->name}}</a>
+
+                <a class="btn btn-danger btn-sm" href="{{ route('logout') }}" onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                    Sair
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+
             </div>
         </div>
 
@@ -21,9 +31,9 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
-                <li class="nav-item">
+                <li class="nav-item menu-open">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <i class="fas fa-arrow-circle-down nav-icon"></i>
                         <p>
                             Entrada
                             <i class="right fas fa-angle-left"></i>
@@ -32,22 +42,22 @@
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
                             <a href="../../index.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
+                                <i class="fas fa-file nav-icon"></i>
                                 <p>Novo Fornecedor</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="../../index2.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Lista de Forncedores</p>
+                                <i class="fas fa-list-alt nav-icon"></i>
+                                <p>Lista de Fornecedores</p>
                             </a>
                         </li>
                     </ul>
                 </li>
 
-                <li class="nav-item">
+                <li class="nav-item menu-open">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <i class="fas fa-arrow-alt-circle-up nav-icon"></i>
                         <p>
                             Saida
                             <i class="right fas fa-angle-left"></i>
@@ -56,22 +66,22 @@
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
                             <a href="../../index.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
+                                <i class="fas fa-file nav-icon"></i>
                                 <p>Novo Cliente</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="../../index2.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
+                                <i class="fas fa-list-alt nav-icon"></i>
                                 <p>Lista de Clientes</p>
                             </a>
                         </li>
                     </ul>
                 </li>
 
-                <li class="nav-item">
+                <li class="nav-item menu-open">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <i class="fas fa-money-check-alt nav-icon"></i>
                         <p>
                             Financeiro
                             <i class="right fas fa-angle-left"></i>
@@ -80,13 +90,13 @@
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
                             <a href="../../index.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
+                                <i class="fas fa-dollar-sign nav-icon"></i>
                                 <p>Novo Lançamento</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="../../index2.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
+                                <i class="fas fa-chart-pie nav-icon"></i>
                                 <p>Relatório Financeiro</p>
                             </a>
                         </li>
