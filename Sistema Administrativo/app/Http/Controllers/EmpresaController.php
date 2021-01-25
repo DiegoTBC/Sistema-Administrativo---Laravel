8b51155e6d031ac710850f541558421f38f53e78
Diego Torres <torressdiiego@outlook.com>
2021-01-25 15:11:26 -0300

    Listagem e paginação dos dados [Funcionando]

--- Sistema Administrativo/app/Http/Controllers/EmpresaController.php
+++ Sistema Administrativo/app/Http/Controllers/EmpresaController.php
@@ -14,7 +14,9 @@ class EmpresaController extends Controller
      */
     public function index()
     {
-        return view('empresa.index');
+        $empresas = Empresa::paginate(10);
+
+        return view('empresa.index', compact('empresas'));
     }
 
     /**
