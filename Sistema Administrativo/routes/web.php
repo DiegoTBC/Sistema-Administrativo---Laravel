c7d8da4722acfecfe7c20e61b941c07bcb7f8d04
Diego Torres <torressdiiego@outlook.com>
2021-01-26 14:23:47 -0300

    Geração do CRUD produtos

--- Sistema Administrativo/routes/web.php
+++ Sistema Administrativo/routes/web.php
@@ -19,3 +19,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('empresas', 'EmpresaController');
+
+Route::resource('produtos', 'ProdutosController');
